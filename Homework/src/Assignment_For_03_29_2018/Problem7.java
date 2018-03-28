32897c3264992f0c18cd620ca4104c3839bcbe2e
frankoffrancis <franklyneze@gmail.com>
2018-03-28 15:31:24 -0400

    Add files via upload

---
+++ Homework/src/Assignment_For_03_29_2018/Problem7.java
@@ -0,0 +1,53 @@
+package Assignment_For_03_29_2018;
+
+public class Problem7 {
+
+    public static void main(String[]args) {
+
+
+        //1
+        // int x=4;
+        for (int i = 0; i < 4; i++) {
+
+            System.out.print("8");
+            for (int j = 0; j < 7; j++) {
+                System.out.print("8");
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        //2
+
+        int count = 4;
+
+        for (int i = 1; i <= count; i++){
+
+            for (int j = count - i + 1; j >= 1; j--){
+
+                System.out.print ("8");
+            }
+
+            System.out.println();
+        }
+
+        //3
+        int sapce = 4;
+
+        for (int i = 1; i <= count; i++){
+
+            for (int j = count - i + 1; j >= 1; j--){
+
+                System.out.print (" ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+    }
+
+

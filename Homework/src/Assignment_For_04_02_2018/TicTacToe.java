f0d7f102d7039c8c425bce8621253735b90e11df
frankoffrancis <franklyneze@gmail.com>
2018-03-30 15:17:01 -0400

    Update TicTacToe.java

--- Homework/src/Assignment_For_04_02_2018/TicTacToe.java
+++ Homework/src/Assignment_For_04_02_2018/TicTacToe.java
@@ -2,12 +2,11 @@ package Assignment_For_04_02_2018;
 
 import java.util.Scanner;
 
-public class TicTacToe {
+public class TicTacToeVer2 {
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
         boolean game = true;
-        String playerOneMark = "x", player2 = "y";
         int row, column;
         String[][] board = new String[3][3];
         for (int i = 0; i < board.length; i++) {
@@ -16,29 +15,35 @@ public class TicTacToe {
                 board[i][j] = " ";
             }
         }
-        while (game == true) {
+        int count=0;
+       mainLabel: while (game == true) {
 
 
-            for (int i = 1; i <= 2; i++) {
-                System.out.println("what row do you want to place your mark Player " + i);
+                System.out.println("what row do you want to place your mark Player " + (count%2+1));
                 row = userInput.nextInt();
-                System.out.println("what column do you want to place your mark player " + i);
+                System.out.println("what column do you want to place your mark player " + (count%2+1));
                 column = userInput.nextInt();
-                if (row < 0 || row > 2 || column < 0 || column > 2) {
-                    i -= 1;
+                if (row < 0 || row > board.length-1 || column < 0 || column > board[0].length-1) {
                     continue;
                 }
 
                 if (board[row][column] != " ") {
                     System.out.println("That place has already been filled please try again");
-                    i -= 1;
                     continue;
-                } else if (i == 1) {
-                    board[row][column] = playerOneMark;
+                } else if (count%2 ==0) {
+                    board[row][column] = "X";
+                    count++;
                 } else {
-                    board[row][column] = player2;
+                    board[row][column] = "Y";
+                    count++;
                 }
-                System.out.println("Current board after Player " + i);
+
+                if(count == board.length*board[0].length){
+
+                    break mainLabel;
+
+                }
+                System.out.println("Current board after Player " + (count%2+1));
                 for (int m = 0; m < board.length; m++) {
                     for (int j = 0; j < board.length; j++) {
                         System.out.print(board[m][j]);
@@ -49,7 +54,9 @@ public class TicTacToe {
 
                 }
 
-            }
+
+            //}
+
 
         }
     }

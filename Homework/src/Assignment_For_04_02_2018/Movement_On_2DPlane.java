75b5e7fab9c731859c3d4b83b34f92165dedb267
frankoffrancis <franklyneze@gmail.com>
2018-03-31 22:52:59 -0400

    Update Movement_On_2DPlane.java

--- Homework/src/Assignment_For_04_02_2018/Movement_On_2DPlane.java
+++ Homework/src/Assignment_For_04_02_2018/Movement_On_2DPlane.java
@@ -1,4 +1,4 @@
-package Assignment_For_04_02_2018;
+package Chp2;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -7,17 +7,20 @@ public class Movement_On_2DPlane {
     public static void main(String[] args) {
 
         Scanner userInput = new Scanner(System.in);
-        String uAnswer =" ";
+        String uAnswer ;
         int row =0 , columns=0;
+        final int LOWEST_BOUNDARY = 0;
+        final int HIGHEST_BOUNDARY = 4;
         char[][]plane = new char[5][5];
         for(int i=0; i <plane.length; i++){
             for(int j=0; j <plane.length; j++) {
 
                 plane[i][j] = '0';
-                }
-
             }
-            plane[0][0] = 'X';
+
+        }
+        plane[row][columns] = 'X';
+
         boolean start = true;
 
         for(int i=0; i <plane.length; i++) {
@@ -34,40 +37,78 @@ public class Movement_On_2DPlane {
 
             uAnswer= userInput.nextLine();
 
-            switch (uAnswer ){
+            switch (uAnswer.toUpperCase() ){
 
-                case "up":
+                case"UP":
+                    if(row-1>=LOWEST_BOUNDARY){
+                    plane[row][columns] = '0';
                     row--;
+                    plane[row][columns] = 'X';
+                }
+                else{
+                        //out of bounds
+                        System.out.println("Out of bound");
+                        start=false;
+                    }
 
-               case "down":
-                   row++;
+                    break;
 
-               case "left":
-                   columns--;
+                case"DOWN":
+                    if(row <= HIGHEST_BOUNDARY){
+                    plane[row][columns] ='0';
+                    row++;
+                    plane[row][columns] ='X';
+                    }
+                    else{
+                        //out of bounds
+                        System.out.println("Out of bound");
+                        start=false;
+                    }
 
-               case "right":
-                   columns++;
+                    break;
+                case"LEFT":
+                    if(columns -1 >=0){
+                        plane[row][columns] ='0';
+                        columns--;
+                        plane[row][columns] = 'X';
+                    }
 
-               default :
+                    else{
+                        //out of bounds
+                        System.out.println("Out of bound");
+                        start=false;
+                    }
+                    break;
+                case"RIGHT":
+                    if(columns <=HIGHEST_BOUNDARY){
+                        plane[row][columns] ='0';
+                        columns++;
+                        plane[row][columns] = 'X';
+                    }
+                    else{
+                        //out of bounds
+                        System.out.println("Out of bound");
+                        start=false;
+                    }
+                    break;
+                default :
                     System.out.println("You entered the wrong direction. Please try again");
                     uAnswer=userInput.nextLine();
 
-            }
-            if(row<0 || row> plane.length || columns<0 || columns> plane[0].length){
-                   System.out.println("You are out of bound please try again");
-                continue ;
-            }
-                
-            }
-            for(int i =0; i<plane.length; i++){
-                for(int j =0; )
-
+                    break;
             }
 
+                for(int i=0; i <plane.length; i++) {
+                    for (int j = 0; j < plane.length; j++) {
 
-
+                        System.out.print(plane[i][j]);
+                    }
+                    System.out.println();
+                }
             }
+
+
+
         }
-
     }
 

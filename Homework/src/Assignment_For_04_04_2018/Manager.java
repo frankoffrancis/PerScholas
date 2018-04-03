e648f6173aaf8da0ba98a6f54ababe8de2f41dbd
frankoffrancis <franklyneze@gmail.com>
2018-04-03 15:25:36 -0400

    Update Manager.java

--- Homework/src/Assignment_For_04_04_2018/Manager.java
+++ Homework/src/Assignment_For_04_04_2018/Manager.java
@@ -2,14 +2,14 @@ package Assignment_For_04_04_2018;
 
 public class Manager extends Employee {
     public Manager(long id, String name, String address, long phone, double salary ){
-       super( id,  name,  address, phone);
+        super( id,  name,  address, phone);
         basicSalary = salary;
     }//@OverRide
-        public void calculateTransportAllowance() {
-            double transportAllowance;
-            transportAllowance = .15 * basicSalary;
-            System.out.println("Employee "+employeeName + " has a transportation allowance of "+transportAllowance);
+    public double calculateTransportAllowance() {
+        double transportAllowance;
+        transportAllowance = .15 * basicSalary;
 
 
+        return transportAllowance;
     }
 }

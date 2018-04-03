9a1ba099440e035865ea5526e06ef8fd475813e6
frankoffrancis <franklyneze@gmail.com>
2018-04-03 13:25:01 -0400

    Update InheritanceActivity.java

--- Homework/src/Assignment_For_04_04_2018/InheritanceActivity.java
+++ Homework/src/Assignment_For_04_04_2018/InheritanceActivity.java
@@ -5,7 +5,11 @@ public class InheritanceActivity {
     public static void main(String[] args){
         Employee manager = new Manager(126534,"Peter","Chennai India",237844,65000 );
         Employee trainee =  new Trainee(29846, "Jack","Mumbai India",442085,45000);
+       //Problem 1
         manager.calculateSalary();
         trainee.calculateSalary();
+        // Problem 2
+        manager.calculateTransportAllowance();
+        trainee.calculateTransportAllowance();
     }
 }

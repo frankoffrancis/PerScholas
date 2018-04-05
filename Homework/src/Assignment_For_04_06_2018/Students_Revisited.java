5ca9f740a10ac1b23da2a434bdc477493279402f
frankoffrancis <franklyneze@gmail.com>
2018-04-05 16:08:58 -0400

    Add files via upload

---
+++ Homework/src/Assignment_For_04_06_2018/Students_Revisited.java
@@ -0,0 +1,52 @@
+package Assignment_For_04_06_2018;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.IOException;
+public class Students_Revisited {
+
+    public static void main(String[] args){
+
+        String namePath = "C:\\Users\\Students\\Desktop\\names.txt";
+        String gradePath = "C:\\Users\\Students\\Desktop\\grades.txt";
+        String gpaPath = "C:\\Users\\Students\\Desktop\\gpa.txt";
+
+
+        File gpa = new File(gpaPath);
+        File names = new File(namePath);
+        File grades = new File(gradePath);
+
+        try {
+            Scanner nameRead = new Scanner(names);
+            Scanner gradesRead = new Scanner(grades);
+            Scanner gpaRead = new Scanner(gpa);
+
+            Student[] studentsName = new Student[10];
+            Student[] studentsGrade = new Student[10];
+            Student[] studentsGPA = new Student[10];
+
+            for(int i=0; i <studentsName.length; i++){
+                studentsName[i] = new Student();
+                studentsGPA[i] = new Student();
+                studentsGrade[i] = new Student();
+
+                studentsName[i].setName(nameRead.nextLine());
+                studentsGrade[i].setGrade(Integer.parseInt(gradesRead.nextLine()));
+                studentsGPA[i].setGpa(Double.parseDouble(gpaRead.nextLine()));
+
+
+
+
+            }
+            for(int i =0; i<studentsName.length; i++){
+                studentsName[i].getInfo();
+            }
+
+        }
+        catch (IOException e){
+            System.out.println("Wrong path");
+        }
+
+    }
+}

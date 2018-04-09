0a6c33676ffd82a960c12863a4adc79ef27017fe
Franklyn Eze <franklyneze@gmail.com>
2018-04-09 16:24:57 -0400

    Update Methods.java

--- Homework/src/Assignment_For_04_10_2018/Methods.java
+++ Homework/src/Assignment_For_04_10_2018/Methods.java
@@ -1,7 +1,7 @@
 package Assignment_For_04_10_2018;
 
-import java.util.ArrayList;
-import java.util.Collections;
+        import java.util.ArrayList;
+        import java.util.Collections;
 
 public class Methods {
 
@@ -15,47 +15,64 @@ public class Methods {
     public String removing(ArrayList<String> array, int index){
 
         return array.remove(index);
-
-
     }
-   public String searching(ArrayList<String> array, String searchingColor ){
+    public String searching(ArrayList<String> array, String searchingColor ){
 
         boolean isFound =false;
         String color =" ";
-           for(String a : array){
-               if(a.equalsIgnoreCase(searchingColor)){
-                   isFound=true;
-               }
+        for(String a : array){
+            if(a.equalsIgnoreCase(searchingColor)){
+                isFound=true;
+            }
 
-           }
-           if(isFound == true ){
-               color =searchingColor;
-           }
-            return color;
-   }
+        }
+        if(isFound == true ){
+            color =searchingColor;
+        }
+        return color;
+    }
 
-   public String update(ArrayList<String> array, int index, String value ){
+    public String update(ArrayList<String> array, int index, String value ){
 
         return array.set(index,value);
-   }
- public void sorting(ArrayList<String> array) {
+    }
+    // get back to it.
+    public void sorting(ArrayList<String> array) {
 
-    Collections.sort(array);
-
- }
-
-
-
- public String reversing(ArrayList<String> array ){
-        String holder= " ";
-    int reverse = array.size()-1;
-
-    for(int i= reverse; i >=0; i--){
-        holder +=array.get(i);
+        Collections.sort(array);
 
     }
-    System.out.println();
-    return holder;
+
+    public String reversing(ArrayList<String> array ){
+        String holder= " ";
+        int reverse = array.size()-1;
+
+        for(int i= reverse; i >=0; i--){
+            holder +=array.get(i);
+
+        }
+        System.out.println();
+        return holder;
+    }
+
+    public ArrayList copying(ArrayList<String> array){
+
+        ArrayList<String> array2 = new ArrayList<>();
+        for(String a : array){
+
+            array2.add(a);
+        }
+        return array2;
+    }
+    public ArrayList comparing(ArrayList<String> a,ArrayList<String> b ){
+        ArrayList<String> holder= new ArrayList<>();
+        for(String c : a ){
+            if(b.contains(c)){
+                holder.add(c);
+
+            }
+        }
+        return holder;
     }
 }
 

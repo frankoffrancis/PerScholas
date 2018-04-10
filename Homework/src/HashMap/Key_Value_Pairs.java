57c22ce753d3b3636f083e8d2c1d7ec8b7b6321f
Franklyn Eze <franklyneze@gmail.com>
2018-04-10 11:20:32 -0400

    Update Key_Value_Pairs.java

--- Homework/src/HashMap/Key_Value_Pairs.java
+++ Homework/src/HashMap/Key_Value_Pairs.java
@@ -11,9 +11,16 @@ public class Key_Value_Pairs {
         firstQuestion.put(3,"Three");
         firstQuestion.put(4,"Four");
         firstQuestion.put(5,"Five");
-
         for(Map.Entry<Integer,String> s : firstQuestion.entrySet()){
             System.out.println(s.getKey()+" "+ s.getValue());
         }
+        System.out.println();
+            firstQuestion.put(2,"The new Star Wars movies are worst than the prequels");
+            firstQuestion.put(4,"The iPhone is an overprice phone");
+        for(Map.Entry<Integer,String> s : firstQuestion.entrySet()){
+            System.out.println(s.getKey()+" "+ s.getValue());
+        } 
     }
+
 }
+        

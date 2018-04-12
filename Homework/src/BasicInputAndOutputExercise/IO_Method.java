ff0139493b6c3b90f7150818bb71904c88bb2bad
Franklyn Eze <franklyneze@gmail.com>
2018-04-12 13:22:59 -0400

    Update IO_Method.java

--- Homework/src/BasicInputAndOutputExercise/IO_Method.java
+++ Homework/src/BasicInputAndOutputExercise/IO_Method.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 public class IO_Method {
 
-    public static void fileSelection(int fileNum){
+    public void fileSelection(int fileNum){
 
         File puzzle;
         int counter =1;
@@ -19,8 +19,9 @@ public class IO_Method {
                       if(counter%3==0){
                           message+= (char)readingPuzzle1.read();
 
-                      }else {
-                          readingPuzzle1.read();
+                      }
+                      else {
+                         readingPuzzle1.read();
                       }
                         counter++;
 
@@ -33,12 +34,36 @@ public class IO_Method {
                 } catch (IOException e) {
                     System.out.println("File not found");
                 }
+                break;
             case 2:
+                puzzle = new File("C:\\Users\\Students\\IdeaProjects\\Homework\\src\\BasicInputAndOutputExercise\\puzzle2.txt");
+                try {
+                    BufferedReader readingPuzzle1= new BufferedReader(new FileReader(puzzle));
+                    while (readingPuzzle1.ready()){
 
+                        if(counter%3==0){
+                            message+= (char)readingPuzzle1.read();
+
+
+                        }else {
+                         readingPuzzle1.read();
+
+                        }
+                        counter++;
+
+                    }
+                    if(readingPuzzle1 !=null){
+                        readingPuzzle1.close();
+
+                        System.out.println("The message is \n"+message);
+                    }
+                } catch (IOException e) {
+                    System.out.println("File not found");
+                }
+                break;
+                default:
+                    System.out.println("Wrong choice");
         }
     }
-    public static void main(String[] args){
 
-        fileSelection(1);
-    }
 }

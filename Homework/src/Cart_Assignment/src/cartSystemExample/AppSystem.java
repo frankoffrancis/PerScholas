3b7636c1edf990f42a3bfd5b859cffe8a3d6cdf5
Franklyn Eze <franklyneze@gmail.com>
2018-04-13 15:44:13 -0400

    Update AppSystem.java

--- Homework/src/Cart_Assignment/src/cartSystemExample/AppSystem.java
+++ Homework/src/Cart_Assignment/src/cartSystemExample/AppSystem.java
@@ -6,10 +6,15 @@ import java.util.HashMap;
 public class AppSystem extends TheSystem {
     public AppSystem() throws FileNotFoundException {
     }
-    /*public void display() {
+ public void display() {
         HashMap<String, Item> current = this.getItemCollection();
         //Fill the code here
+      for(String s : current.keySet()){
+          System.out.print(s);
+      }
+
     }
+ /*
     public Boolean add(Item item) {
         //Fill the code here
     }*/

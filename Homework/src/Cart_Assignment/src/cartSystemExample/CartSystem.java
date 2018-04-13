106b0b2219de8a175b8d3163e81f5b3fd0288756
Franklyn Eze <franklyneze@gmail.com>
2018-04-13 16:52:01 -0400

    Update CartSystem.java

--- Homework/src/Cart_Assignment/src/cartSystemExample/CartSystem.java
+++ Homework/src/Cart_Assignment/src/cartSystemExample/CartSystem.java
@@ -2,11 +2,27 @@ package cartSystemExample;
 
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Map;
 
 public class CartSystem extends TheSystem{
     public CartSystem() throws FileNotFoundException {
     }
     public void display() {
       //Fill the code here
+        double total=0;
+        double subTotal=0;
+        double tax=0;
+        double taxPlusSubtotal=0;
+        for(Map.Entry<String, Item> item: this.getItemCollection().entrySet()){
+            subTotal = item.getValue().getQuatity()* item.getValue().getItemPrice();
+            tax = subTotal*.05;
+            taxPlusSubtotal = tax+subTotal;
+            total +=taxPlusSubtotal;
+
+            System.out.println(item.getKey()+"    "+item.getValue().getItemDesc()+"    "+item.getValue().getQuatity()+"    "+taxPlusSubtotal);
+
+        }
+        System.out.println();
     }
+
 }

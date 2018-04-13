36bd66e06ad9ee5d3e72b3520bd996796860c5e5
Franklyn Eze <franklyneze@gmail.com>
2018-04-13 15:23:19 -0400

    Update TheSystem.java

--- Homework/src/Cart_Assignment/src/cartSystemExample/TheSystem.java
+++ Homework/src/Cart_Assignment/src/cartSystemExample/TheSystem.java
@@ -11,31 +11,30 @@ public class TheSystem {
 
     protected TheSystem() throws FileNotFoundException {
         itemCollection = new HashMap<String, Item>();
-        String [] lines ;
+        String[] lines;
         //Fill the code here
-        if(getClass().getSimpleName().equals("AppSystem")){
+        if (getClass().getSimpleName().equals("AppSystem")) {
             File itemFiles = new File("sample.txt");
-            BufferedReader readingItemsFile= new BufferedReader(new FileReader(itemFiles));
+            BufferedReader readingItemsFile = new BufferedReader(new FileReader(itemFiles));
             try {
-                while(readingItemsFile.ready()) {
+                while (readingItemsFile.ready()) {
 
                     String line = readingItemsFile.readLine();
 
-                      Item item = readingFromLine(line);
+                    Item item = readingFromLine(line);
                     System.out.println(item);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else{
+        } else {
 
         }
     }
 
     private Item readingFromLine(String line) {
 
-        String [] holdingOptions =line.split("  ");
+        String[] holdingOptions = line.split("  ");
         Item options = new Item();
         options.setItemName(holdingOptions[0]);
         options.setItemDesc(holdingOptions[1]);
@@ -46,29 +45,52 @@ public class TheSystem {
 
         options.setAvailableQuatity(Integer.parseInt(holdingOptions[3]));
 
-        return  options;
+        return options;
 
     }
-       public HashMap<String, Item> getItemCollection(){
-      //Fill the code here
+
+    public HashMap<String, Item> getItemCollection() {
+        //Fill the code here
 
 
-           return new HashMap<>(itemCollection);
+        return new HashMap<>(itemCollection);
     }
 
-    public void setItemCollection(HashMap<String, Item> copy ){
-      //Fill the code her
-        itemCollection =copy;
+    public void setItemCollection(HashMap<String, Item> copy) {
+        //Fill the code her
+        itemCollection = copy;
     }
-     /*
+
     public Boolean add(Item item) {
-      //Fill the code here
+        //Fill the code here
+        if (this.itemCollection.containsKey(item.getItemName())) {
+            Item Curernt = itemCollection.get(item.getItemName());
+            if (Curernt.getAvailableQuatity() <= Curernt.getQuatity()) {
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            itemCollection.put(item.getItemName(), item);
+            return true;
+        }
     }
 
     public Item remove(String itemName) {
-        Item item = null;
-      //Fill the code here
-    }}*/
+        //Fill the code here
+        if (itemCollection.containsKey(itemName)) {
+            return itemCollection.remove(itemName);
+        } else {
+
+             Item item = null;
+
+             return item;
+        }
+    }
+
+  /*
+    */
     public Boolean checkAvailability(Item item, Integer current) {
       //Fill the code here
         if((item.getQuatity()+current)>item.getAvailableQuatity()){

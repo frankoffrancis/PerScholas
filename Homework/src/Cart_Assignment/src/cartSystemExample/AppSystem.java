1b619a01cc42cd3e2ee967b5b4cd1f9f2648cce3
Franklyn Eze <franklyneze@gmail.com>
2018-04-14 10:09:44 -0400

    Update AppSystem.java

--- Homework/src/Cart_Assignment/src/cartSystemExample/AppSystem.java
+++ Homework/src/Cart_Assignment/src/cartSystemExample/AppSystem.java
@@ -8,35 +8,37 @@ public class AppSystem extends TheSystem {
     public AppSystem() throws FileNotFoundException {
 
     }
+
     public void display() {
 
         HashMap<String, Item> current = this.getItemCollection();
         //Fill the code here
-        for(Item s : current.values()){
-         Integer itemAvailable= s.getAvailableQuatity();
-            System.out.println("The current available choices are "+s.getItemName()+" "+s.getItemDesc()+
-                    " "+s.getItemPrice()+""+s.getAvailableQuatity());
+        for (Item s : current.values()) {
+            Integer itemAvailable = s.getAvailableQuatity();
+            System.out.println("The current available choices are " + s.getItemName() + " " + s.getItemDesc() +
+                    " " + s.getItemPrice() + "" + s.getAvailableQuatity());
         }
 
     }
 
-    public Boolean add(Item item)  {
+    public Boolean add(Item item) {
         //Fill the code here
         boolean isAdded = false;
 
-            if (this.getItemCollection().containsKey(item.getItemName())) {
+        if (this.getItemCollection().containsKey(item.getItemName())) {
 
-                System.out.println("Item "+item.getItemName()+ " is already in the system");
+            System.out.println("Item " + item.getItemName() + " is already in the system");
 
-                isAdded= false;
+            isAdded = false;
 
-            }
-            else {
-                HashMap<String, Item> addItems = this.getItemCollection();
-                addItems.put(item.getItemName(),item);
-                this.setItemCollection(addItems);
-                isAdded=true;
         }
-        return isAdded;
+
+            HashMap<String, Item> addItems = this.getItemCollection();
+            addItems.put(item.getItemName(), item);
+            this.setItemCollection(addItems);
+            isAdded = true;
+            return isAdded;
+
+
     }
 }

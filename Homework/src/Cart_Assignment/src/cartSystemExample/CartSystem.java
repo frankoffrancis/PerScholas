65940cf055b50f2fda393a9f81ba0d22c56d689b
Franklyn Eze <franklyneze@gmail.com>
2018-04-15 12:54:16 -0400

    Update CartSystem.java

--- Homework/src/Cart_Assignment/src/cartSystemExample/CartSystem.java
+++ Homework/src/Cart_Assignment/src/cartSystemExample/CartSystem.java
@@ -13,12 +13,12 @@ public class CartSystem extends TheSystem{
         double subTotal=0;
         double tax=0;
         double taxPlusSubtotal=0;
+        System.out.printf("|%20s%20s%20s%20s%20s  | ","Item Name", "Item Quantity","Sub Total","Tax","Total\n");
         for(Map.Entry<String, Item> item: this.getItemCollection().entrySet()){
             subTotal = item.getValue().getQuatity()* item.getValue().getItemPrice();
             tax = subTotal*.05;
             total = tax+subTotal;
 
-            System.out.printf("|%20s%20s%20s%20s%20s  | ","Item Name", "Item Quantity","Sub Total","Tax","Total"); 
             System.out.printf("|%20s%20s%20s%20s%20s  | ",item.getValue().getItemName(),item.getValue().getQuatity(),+subTotal ,tax ,total);
 
         }

f4764c3a0ac7c73a57a63eaa7d1ff68cf54a896f
Franklyn Eze <franklyneze@gmail.com>
2018-05-03 15:28:16 -0400

    Update Order.java

--- Homework/src/JDBCExcerise2/src/e2/models/Order.java
+++ Homework/src/JDBCExcerise2/src/e2/models/Order.java
@@ -5,5 +5,24 @@ public class Order {
 	private Integer orderID;
 	private Integer itemID;
 	private Integer Quantity;
+	
+	public Integer getOrderID() {
+		return orderID;
+	}
+	public void setOrderID(Integer orderID) {
+		this.orderID = orderID;
+	}
+	public Integer getItemID() {
+		return itemID;
+	}
+	public void setItemID(Integer itemID) {
+		this.itemID = itemID;
+	}
+	public Integer getQuantity() {
+		return Quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		Quantity = quantity;
+	}
 
 }

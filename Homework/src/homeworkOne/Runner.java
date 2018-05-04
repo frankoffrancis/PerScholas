83ea97adb798416aa6965162585f3493ac7d8e86
Franklyn Eze <franklyneze@gmail.com>
2018-05-04 09:09:43 -0400

    Update Runner.java

--- Homework/src/homeworkOne/Runner.java
+++ Homework/src/homeworkOne/Runner.java
@@ -1,11 +1,27 @@
 package homeworkOne;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class Runner {
 
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		
 		ItemDAO item = new ItemDAO();
-		item.getItemID(2);
-	}
+		List<Item> item1 = new ArrayList<Item>();
+		item1 = item.getItemsInStock();
+		
+		for(Item i : item1) {
+			System.out.println(i.getItem_id());
+			System.out.println(i.getIteamName());
+			System.out.println(i.getItemPrice());
+			System.out.println(i.getItemInStock());
+			
 	
+		}
+	
+
+	}
 }

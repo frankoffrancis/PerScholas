29db6e1051526856eaf66a3453b6e06fffc6e186
Franklyn Eze <franklyneze@gmail.com>
2018-05-06 20:32:20 -0400

    Update OracleQueries.java

--- Homework/src/CoreJavaJDBC_Assessment/src/CoreJava/utils/OracleQueries.java
+++ Homework/src/CoreJavaJDBC_Assessment/src/CoreJava/utils/OracleQueries.java
@@ -5,31 +5,33 @@ public class OracleQueries {
 	public static final String GETSTUDENTBYGMAIL = "Select * from student "
 			+ "where email = ?"; 
 
-	public static final String GETALLINSTRUCTOR = "Select * from instructor";
+	public static final String GETALLINSTRUCTOR = "Select * from instructor ";
 	
 	public static final String GETINSTRUCTORBYGMAIL = "Select * from instructor "
 			+ "where email = ?";
 	
 	public static final String GETALLCOURSES = "select * from course"; 
 	
-	public static final String GETCOURSESBYINSTRUCTORID = "select distinct COURSE.COURSE_ID, COURSE.COURSE_NAME, COURSE.MINIMUN_GPA from  course "
-			+ " join teaching on course.course_id = teaching.course_id "
-			+ "where teaching.instructor_id = ?";
+	public static final String GETCOURSESBYINSTRUCTORID = "select distinct COURSE.COURSE_ID, COURSE.COURSE_NAME, COURSE.MINIMUN_GPA FROM COURSE  \r\n" + 
+			"         join teaching on course.course_id = teaching.course_id \r\n" + 
+			"			   where teaching.instructor_id =?";
 	
 	public static final String REGISTERSTUDENTTOCOURSE = "insert into attending "
 			+ " (course_id, student_id) "
 			+ "values (?,?)";
-	public static final String GETSTUDENTCOURSE = "select course.course_id, course.course_name, instructor.full_name, instructor.email "
-			+ "from attending inner join course on  attending.course_id = course.course_id "
-			+ "inner join teaching on course.course_id = teaching.course_id "
-			+ "inner join instructor on teaching.instructor_id = instructor.instructor_id "
-			+ "where attending.student_id = ? ";
+	public static final String GETSTUDENTCOURSE = "Select distinct course.course_name, instructor.full_name, instructor.email " + 
+			"      from course inner join attending on attending.course_id = COURSE.course_id " + 
+			"      inner join teaching on teaching.COURSE_ID = attending.COURSE_ID " + 
+			"      inner JOIN INSTRUCTOR on teaching.INSTRUCTOR_ID = INSTRUCTOR.INSTRUCTOR_ID " + 
+			"      inner join student on student.STUDENT_ID = attending.STUDENT_ID " + 
+			"      where attending.STUDENT_ID=?";
 	
 	public static final String ASSIGNTEACHERTOCOURSE  = "INSERT into teaching (course_id, instructor_id) "
 			+ "values(?,?)"; 
 	
-	public static final String GETTEACHER = "select * from teaching "
+	public static final String GETTEACHER = "select course.course_name, course.minimun_gpa , instructor.full_name , instructor.email from instructor "
 			+ "inner join teaching on instructor.instructor_id = teaching.instructor_id "
-			+ "inner join course on teaching.course_id = course.course_id";
+			+ "inner join course on teaching.course_id = course.course_id "
+			;
 	
 }
